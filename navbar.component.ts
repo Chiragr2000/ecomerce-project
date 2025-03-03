d6ee2c56341c11076aa831d7522e6aee346d78f1
Chirag Reddy <chiragreddy540@gmail.com>
2025-03-03 11:14:52 +0530

    Add files via upload

---
+++ navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { DataStorageService } from '../service/data-storage.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit{
+
+  constructor(private dataStorage: DataStorageService) {}
+  cartCount: number=0;
+
+  @Input() cartcount: number=0;
+  ngOnInit(): void {
+    var getVal = this.dataStorage.getCartData();
+    this.cartCount = getVal ? getVal.length : 0;
+  }
+
+}
